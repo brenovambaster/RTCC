dc5fdc6a9d1341dbcdf2d6240ecc0e943a14d12b
Breno Vambaster <bvcl@aluno.ifnmg.edu.br>
2024-08-02 11:26:31 -0300

    test

---
+++ demo/src/test/java/com/rtcc/demo/RtccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtcc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
