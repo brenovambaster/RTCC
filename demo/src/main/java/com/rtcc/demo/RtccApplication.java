4be024e241763baba0dc1e439bf4dd1307692e70
Filipe Abner <filipeabner18@gmail.com>
2024-08-08 12:15:18 -0300

    :heavy_plus_sign: build: dependencies and config for file management

--- demo/src/main/java/com/rtcc/demo/RtccApplication.java
+++ demo/src/main/java/com/rtcc/demo/RtccApplication.java
@@ -1,12 +1,15 @@
 package com.rtcc.demo;
 
+import com.rtcc.demo.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class RtccApplication {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		SpringApplication.run(RtccApplication.class, args);
 	}
 
