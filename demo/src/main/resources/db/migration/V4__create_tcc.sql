c4255144c0ff92f5bf2f882d32ef8444f5458c5b
Breno Vambaster <bvcl@aluno.ifnmg.edu.br>
2024-08-16 18:52:36 -0300

    ♻️ refactor(TCC): altera a tabela tcc. cria uma tabela apenas para as palavras chaves. um Tcc pode ter 1:N palavras chaves

--- demo/src/main/resources/db/migration/V4__create_tcc.sql
+++ demo/src/main/resources/db/migration/V4__create_tcc.sql
@@ -8,7 +8,6 @@ CREATE TABLE tcc
     advisor      TEXT             NOT NULL,
     summary      TEXT             NOT NULL,
     abstract     TEXT             NOT NULL,
-    keywords     TEXT             NOT NULL,
     language     VARCHAR(50)      NOT NULL,
     path_file    VARCHAR(255)     NOT NULL,
     CONSTRAINT fk_course FOREIGN KEY (course) REFERENCES course (id),
@@ -27,3 +26,17 @@ CREATE TABLE tcc_committee_members
 
 -- ID está como TEXT pois O banco de dados não precisa saber que é um UUID.
 -- Além disso, se migrar para outro banco de dados, o UUID pode ser gerado de outra forma ou não seja suportado.
+
+CREATE TABLE keywords
+(
+    name VARCHAR(255) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE tcc_keywords
+(
+    tcc_id       TEXT         NOT NULL,
+    keyword_name VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_tcc_keywords PRIMARY KEY (tcc_id, keyword_name),
+    CONSTRAINT fk_tcc FOREIGN KEY (tcc_id) REFERENCES tcc (id),
+    CONSTRAINT fk_keyword FOREIGN KEY (keyword_name) REFERENCES keywords (name)
+);
